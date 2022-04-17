7f98045152a9a851f723494d4eb2d1d05af6747c
csfx-py <csfxshreyas@gmail.com>
2022-04-17 23:13:38 +0530

    showing number of users and sims, formatting ref string

--- results.php
+++ results.php
@@ -7,16 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Swapping Algorithms Stats</title>
     <link rel="stylesheet" href="./css/res.css">
+    <link href="https://fonts.googleapis.com/css2?family=Beau+Rivage&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <div class="header">Paging and Page Replacement Algorithms</div>
-
+    <h1>
+        Results for Page Swapping Algorithms by users
+    </h1>
     <?php
     require_once "./server/config.php";
     $sql = "SELECT *, name FROM stats join student on stats.srn = student.srn";
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
     if ($resultCheck > 0) {
+        echo "<h2>Number of registered users: " . mysqli_num_rows(mysqli_query($conn, "SELECT * FROM student")) . "</h2>";
+        echo "<h2>Number of times simuation saved: " . $resultCheck . "</h2>";
         echo "<table>";
         echo "<tr>";
         echo "<th>SRN</th>";
@@ -33,7 +39,7 @@
         echo "</tr>";
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
-            echo "<td>" . $row['srn'] . "<br />" . $row['name'] . "</td>";
+            echo "<td>" . $row['srn'] . "<br /><b>" . $row['name'] . "</b></td>";
             echo "<td>" . $row['ref_string'] . "</td>";
             echo "<td>" . $row['proc_count'] . "</td>";
             echo "<td>" . $row['ram_size'] . "KB</td>";
@@ -51,6 +57,30 @@
         echo "No results found";
     }
     ?>
+    <script>
+        // for each row in table, get 2nd column
+        document.querySelectorAll('tr').forEach(function(row) {
+            // get 2nd column
+            var col = row.querySelectorAll('td')[1];
+            // if 2nd column is not empty
+            if (col?.innerHTML) {
+                // get the value of 2nd column
+                var val = col.innerHTML;
+                // split by comma
+                var arr = val.split(',');
+                // put br tag after every 10 elements
+                for (var i = 1; i < arr.length; i++) {
+                    if (i % 10 == 0) {
+                        arr[i - 1] = arr[i - 1] + ' |<br />      ';
+                    }
+                }
+                // join the array
+                var newVal = arr.join(' | ');
+                // replace the value of 2nd column
+                col.innerHTML = ' | ' + newVal + ' | ';
+            }
+        });
+    </script>
 </body>
 
 </html>
